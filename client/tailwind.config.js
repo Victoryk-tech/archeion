9d927afb777a163d263c41d86565fdac0501d1fc
VictoryK-tech <kemelevictory3802@gmail.com>
2024-10-01 11:35:20 +0100

    font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      Libre: ["Libre Baskerville", " serif"],
+      inter: ["Inter", " sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
